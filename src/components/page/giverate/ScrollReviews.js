da2f4aa6341de123046d35e631c255a055ef0833
Konrad Kowalczyk <konrad.kowalczyk.98@gmail.com>
2020-09-25 10:39:58 +0200

    Add precompiled assets for Heroku

--- src/components/page/giverate/ScrollReviews.js
+++ src/components/page/giverate/ScrollReviews.js
@@ -60,9 +60,9 @@ export default class ScrollReview extends Component {
   render() {
     const { error, isLoaded, items } = this.state;
     if (error) {
-      return <div>Błąd: {error.message}</div>;
+      return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
-      return <div>Ładowanie...</div>;
+      return <div>Loading...</div>;
     } else {
       return (
         <div className="layoutScroll">
