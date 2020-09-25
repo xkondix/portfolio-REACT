3cf57798dd21993e2e4608b3ddf9d3cda0705c36
Konrad Kowalczyk <konrad.kowalczyk.98@gmail.com>
2020-09-25 20:33:32 +0200

    page give me rate

--- src/components/page/contact/Contact.js
+++ src/components/page/contact/Contact.js
@@ -1,5 +1,10 @@
 import React, {useEffect, useState} from "react";
 import './Contact.css';
+import linkedlin from './linkedlin.png'
+import github from './github.png'
+import email from './email.png'
+import cv from './cv.png'
+import myPDF from './Konrad_Kowalczyk_CV.pdf';
 
 
 export default function Conteact({ title,id }) {
@@ -35,6 +40,33 @@ export default function Conteact({ title,id }) {
       <div className="section-content" id={id}>
         <h1 >
           {title}</h1>
+
+          <div className="wrapper">
+            <figure className = 'gallery1'>
+
+            </figure>
+            <figure className = 'gallery2'>
+                <a href="konrad.kowalczyk.98@gmail.com">
+                    <img src = {email} className='gallery__img' alt="error"></img>
+                </a>
+            </figure>
+            <figure className = 'gallery3'>
+              <a href="https://www.linkedin.com/in/konrad-kowalczyk-3a621218b/">
+                  <img src = {linkedlin} className='gallery__img' alt="error"></img>
+              </a>
+            </figure>
+            <figure className = 'gallery4'>
+              <a href="https://www.github.com/xkondix">
+                  <img src = {github}  className='gallery__img' alt="error"></img>
+              </a>
+            </figure>
+            <figure className = 'gallery5'>
+              <a href={myPDF} download="cv.pdf">
+                 <img src = {cv} className='gallery__img' alt="error" ></img>
+              </a>
+            </figure>
+
+            </div>
           
       </div>
     </div>
