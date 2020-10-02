c4894ca80da9cb4016512c4cb30f7bd4434b2803
Konrad Kowalczyk <konrad.kowalczyk.98@gmail.com>
2020-10-02 21:22:53 +0200

    page give me rate 6

--- src/components/page/projects/Information.js
+++ src/components/page/projects/Information.js
@@ -2,8 +2,8 @@
 export const Information = [
     {
         title : 'hurtownia JAVA-SPRING',
-        describe : 'Create full rest project of wholesae in spring. In project i use HATEOASE, HIBERNATE, Spring-Boot and MySQL.',
-        src :"https://www.youtube.com/watch?v=KkhGkRahU6g",
+        describe : 'Create full rest project of wholesae in spring. In project i use HATEOASE, HIBERNATE, Spring-Boot, Seciurity, MySQL, and other dependencies.',
+        src :"https://www.youtube.com/watch?v=xN-6oQTAoy8",
         link : "https://github.com/xkondix/hurtownia-JAVA-SPRING"
         },
     {
